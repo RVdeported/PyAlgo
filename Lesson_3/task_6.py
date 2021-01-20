4657374ce4b74d88bd9a6f173d661f57d49050a7
RVdeported <romanvetrin@gmail.com>
2021-01-20 16:17:44 +0300

    other

--- Lesson_3/task_6.py
+++ Lesson_3/task_6.py
@@ -9,29 +9,30 @@ Created on Mon Jan 18 18:43:28 2021
 минимальным и максимальным элементами. Сами минимальный и 
 максимальный элементы в сумму не включать.
 """
+import random as r
 
-nums = [r.randint(-99,99) for n in range(10)]
+def minimax(n):
+    nums = [r.randint(-99,99) for n in range(n)]
+    low_num_i = 0
+    large_num_i = 0
+    # finding low and high indexes
+    for i, n in enumerate(nums):
+        if (n < nums[low_num_i]):
+            low_num_i = i
+        if (n > nums[large_num_i]):
+            large_num_i = i
+    # change indexes so low is start and high is end
+    if (low_num_i > large_num_i):
+        buff = low_num_i
+        low_num_i = large_num_i
+        large_num_i = buff
+    summ = 0
+    for n in nums[low_num_i + 1: large_num_i]:
+        summ += n
+    return summ
 
-low_num_i = 0
-large_num_i = 0
-# finding low and high indexes
-for i, n in enumerate(nums):
-    if (n < nums[low_num_i]):
-        low_num_i = i
-    if (n > nums[large_num_i]):
-        large_num_i = i
-# change indexes so low is start and high is end
-if (low_num_i > large_num_i):
-    buff = low_num_i
-    low_num_i = large_num_i
-    large_num_i = buff
 
-summ = 0
-for n in nums[low_num_i + 1: large_num_i]:
-    print(f'adding {n}')
-    summ += n
-
-print("Array of random elements:\n")
-print(nums)
-print(f'Total sum is {summ}')
-print(f'Start index is {low_num_i}, end index is {large_num_i} with values {nums[low_num_i]} and {nums[large_num_i]}')
+res = minimax(100)
+print(res)
+#print(f'Total sum is {summ}')
+#print(f'Start index is {low_num_i}, end index is {large_num_i} with values {nums[low_num_i]} and {nums[large_num_i]}')
