a21ee7f6cfee8e1771e6742d09e1d847ca18be44
RVdeported <romanvetrin@gmail.com>
2021-01-24 13:49:58 +0300

    bugs

--- Lesson_5/task_1.py
+++ Lesson_5/task_1.py
@@ -19,13 +19,13 @@ company = namedtuple('comp',["name","income"])
 companies = []
 
 while(True):
-    inp = input("Please, enter name and income of a company via space to add it or press '-1' to exit: ").split(" ")
+    inp = input("Please, enter name and income (for each quarter) of a company via space to add it or press '-1' to exit.\nFor example: 'Apple 100 200 300 400'\n").split(" ")
     if (inp [0] == "-1"):
         break
-    companies.append(company(inp[0],int(inp[1])))
+    companies.append(company(inp[0],[int(i) for i in inp[1:]]))
 
-average_inc = sum([i.income for i in companies]) / len(companies)
-above_avg = [i.income > average_inc for i in companies]
+average_inc = sum([sum(i.income) for i in companies]) / len(companies)
+above_avg = [sum(i.income) > average_inc for i in companies]
 higher_comp = [i.name for ind, i in enumerate(companies) if above_avg[ind]]
 lower_comp =  [i.name for ind, i in enumerate(companies) if not above_avg[ind]]
 print(f'Average income is {average_inc}.')
